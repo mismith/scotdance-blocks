86b11716211de09ffc608e5288d91dc1a517fe1f
Murray Rowan <murray@mismith.info>
2026-02-27 13:59:01 -0700

    feat: add undo/redo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,46 @@
 <script setup lang="ts">
 import { DnDProvider } from '@vue-dnd-kit/core'
+import { useMagicKeys, useRefHistory, whenever } from '@vueuse/core'
+import { storeToRefs } from 'pinia'
 import { RouterView } from 'vue-router'
+
+import { useCompetitionStore } from '@/stores/competition'
+
+const store = useCompetitionStore()
+const { data } = storeToRefs(store)
+
+const { undo, redo } = useRefHistory(data, {
+  deep: true,
+  capacity: 100,
+  clone: (val) => JSON.parse(JSON.stringify(val)),
+})
+
+function isEditingText() {
+  const el = document.activeElement
+  return el instanceof HTMLInputElement || el instanceof HTMLTextAreaElement || (el instanceof HTMLElement && el.isContentEditable)
+}
+
+const keys = useMagicKeys()
+whenever(
+  () => keys['Meta+z'].value && !isEditingText(),
+  () => undo(),
+)
+whenever(
+  () => keys['Ctrl+z'].value && !isEditingText(),
+  () => undo(),
+)
+whenever(
+  () => keys['Meta+Shift+z'].value && !isEditingText(),
+  () => redo(),
+)
+whenever(
+  () => keys['Ctrl+Shift+z'].value && !isEditingText(),
+  () => redo(),
+)
+whenever(
+  () => keys['Ctrl+y'].value && !isEditingText(),
+  () => redo(),
+)
 </script>
 
 <template>
