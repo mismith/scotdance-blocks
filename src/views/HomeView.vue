e426f4305715289d91ed40c30602c759fbd5c452
Murray Rowan <murray@mismith.info>
2026-03-22 22:58:16 -0600

    feat: simplify name to just Blocks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,7 +28,7 @@ function handleNew() {
     </div>
 
     <TouchIcon class="size-20 rounded-2xl" />
-    <h1 class="mt-6 text-4xl font-bold text-foreground">ScotDance Blocks</h1>
+    <h1 class="mt-6 text-4xl font-bold text-foreground">Blocks</h1>
     <p class="mt-3 text-lg text-muted-foreground">
       Schedule builder for Highland dance competitions
     </p>
